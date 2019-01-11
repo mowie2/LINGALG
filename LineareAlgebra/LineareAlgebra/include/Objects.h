c5a17f6a1351679168571ea0814de5c6e1a5637a
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2019-01-11 13:15:58 +0100

    object->square

---
+++ LineareAlgebra/LineareAlgebra/include/Objects.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+#include "Matrix3f.h"
+
+class Objects
+{
+public:
+	static std::vector<Matrix3f> cube();
+};
+
--- LineareAlgebra/LineareAlgebra/src/Objects.cpp
+++ LineareAlgebra/LineareAlgebra/src/Objects.cpp
@@ -1,8 +1,6 @@
-#include "../include/SpaceShip.h"
+#include "../include/Objects.h"
 
-
-
-SpaceShip::SpaceShip()
+std::vector<Matrix3f> Objects::cube()
 {
 	Matrix3f square;
 	square.AddVector(
@@ -48,12 +46,5 @@ SpaceShip::SpaceShip()
 	matrices.push_back(square4);
 	matrices.push_back(square5);
 	matrices.push_back(square6);
-	shape_ = Shape(matrices, Vector3f(.5, .5, .5));
-	shape_.translate(Vector3f(-.5, -.5, -.5));
-	shape_.rotate(Vector3f(0, 0, 45));
-	//shape_.translate(Vector3f(1, 2, 0));
+	return matrices;
 }
-
-
-SpaceShip::~SpaceShip()
-= default;
