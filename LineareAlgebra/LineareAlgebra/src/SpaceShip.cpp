61c1d5a06fd150122a7986e0eda84e7b305a822f
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2019-01-11 14:04:52 +0100

    removed code

--- LineareAlgebra/LineareAlgebra/src/SpaceShip.cpp
+++ LineareAlgebra/LineareAlgebra/src/SpaceShip.cpp
@@ -4,53 +4,8 @@
 
 SpaceShip::SpaceShip()
 {
-	shape_ = new Shape(Objects::cube(), Vector3f{ 0,0,0 });
+	shape_ = Shape(Objects::cube(), Vector3f{ 0,0,0 });
 
-	//Matrix3f square;
-	//square.AddVector(
-	//	Vector3f(-0.5, -0.5, 0.5),
-	//	Vector3f(0.5, -0.5, 0.5),
-	//	Vector3f(0.5, 0.5, 0.5),
-	//	Vector3f(-0.5, 0.5, 0.5));
-	//Matrix3f square2;
-	//square2.AddVector(
-	//	Vector3f(0.5, -0.5, -0.5),
-	//	Vector3f(-0.5, -0.5, -0.5),
-	//	Vector3f(-0.5, 0.5, -0.5),
-	//	Vector3f(0.5, 0.5, -0.5));
-	//Matrix3f square3;
-	//square3.AddVector(
-	//	Vector3f(0.5, -0.5, 0.5),
-	//	Vector3f(-0.5, -0.5, 0.5),
-	//	Vector3f(-0.5, -0.5, -0.5),
-	//	Vector3f(0.5, -0.5, -0.5));
-	//Matrix3f square4;
-	//square4.AddVector(
-	//	Vector3f(0.5, 0.5, 0.5),
-	//	Vector3f(0.5, 0.5, -0.5),
-	//	Vector3f(-0.5, 0.5, -0.5),
-	//	Vector3f(-0.5, 0.5, 0.5));
-	//Matrix3f square5;
-	//square5.AddVector(
-	//	Vector3f(-0.5, -0.5, 0.5),
-	//	Vector3f(-0.5, 0.5, 0.5),
-	//	Vector3f(-0.5, 0.5, -0.5),
-	//	Vector3f(-0.5, -0.5, -0.5));
-	//Matrix3f square6;
-	//square6.AddVector(
-	//	Vector3f(0.5, -0.5, -0.5),
-	//	Vector3f(0.5, 0.5, -0.5),
-	//	Vector3f(0.5, 0.5, 0.5),
-	//	Vector3f(0.5, -0.5, 0.5));
-	//
-	//std::vector<Matrix3f> matrices;
-	//matrices.push_back(square);
-	//matrices.push_back(square2);
-	//matrices.push_back(square3);
-	//matrices.push_back(square4);
-	//matrices.push_back(square5);
-	//matrices.push_back(square6);
-	//shape_ = Shape(matrices, Vector3f(.5, .5, .5));
 	//shape_.translate(Vector3f(-.5, -.5, -.5));
 	//shape_.rotate(Vector3f(0, 0, 45));
 	//shape_.translate(Vector3f(1, 2, 0));
