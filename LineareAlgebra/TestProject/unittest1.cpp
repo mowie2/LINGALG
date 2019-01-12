420cc33c0acb73f6c1d592af39caa363c000e215
Mohamed <mhamoudi@avans.nl>
2019-01-12 13:25:02 +0100

    Setup Unittesting

---
+++ LineareAlgebra/TestProject/unittest1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../LineareAlgebra/include/Physics.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestProject
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(Physics_collision_test)
+		{
+			// TODO: Your test code here
+
+			
+			Assert::AreEqual(1, 1);
+		}
+
+	};
+}
