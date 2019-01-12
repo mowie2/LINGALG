389e6c4cbbf473cdad8f3c3034dac879bed80413
Mohamed <mhamoudi@avans.nl>
2019-01-12 14:38:11 +0100

    Setup dependencies

--- LineareAlgebra/TestProject/unittest1.cpp
+++ LineareAlgebra/TestProject/unittest1.cpp
@@ -1,20 +1,24 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../LineareAlgebra/include/Physics.h"
+#include "../LineareAlgebra/include/Vector3f.h"
+
+//#include "../LineareAlgebra/include/Physics.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TestProject
-{		
+{
+
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		TEST_METHOD(Physics_collision_test)
 		{
 			// TODO: Your test code here
-
 			
+
 			Assert::AreEqual(1, 1);
 		}
 
